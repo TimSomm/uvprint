97f1bdef5158df5824dfa988350b11b12574893a
timsomm <somatimer2000@gmail.com>
2022-01-17 13:44:45 +0100

    removed unnecessary imports

--- src/main.js
+++ src/main.js
@@ -1,18 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueGeolocation from 'vue-browser-geolocation';
-import * as VueGoogleMaps from 'vue2-google-maps';
-
-Vue.use(VueGeolocation);
-
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyBJQbmKemAm5KTJVlSjXWWMpU1qcxeab_U',
-  },
-  installComponents: false,
-});
-
-Vue.component('google-map', VueGoogleMaps.Map);
 
 Vue.config.productionTip = false;
 
